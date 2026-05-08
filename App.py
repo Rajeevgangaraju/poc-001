244f48cee4d80bab30a6def9afae56e6d22230b8
Rajeevgangaraju <134952680+Rajeevgangaraju@users.noreply.github.com>
2026-05-09 02:15:16 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return "<h1>Python POC: Pipeline Successful!</h1><p>Flask Backend is running.</p>"
+    return "Python POC: Pipeline Successful! Flask Backend is running."
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
